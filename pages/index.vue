d87a57a892b5ccb46099ef7ad62df1bd4181ee16
Julio Quiezi <julioquiezi@gmail.com>
2024-12-07 15:46:37 +0900

    feat: add todo page

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+
+
+<template>
+    <div>
+        <h1>Todo Page</h1>
+        <ul>
+            <li v-for="(item, index) in todos" :key="index">{{ item }}</li>
+        </ul>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { ref } from "vue"
+    const todos = ref(["Buy groceries", "Study Vue", "Walk my dog"])
+</script>
+
+<style scoped>
+    h1 {
+        font-size: 2rem;
+        margin-bottom: 1rem;
+    }
+</style>
